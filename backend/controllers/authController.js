0351b28683bfc04cb0cfa689e5ce87c7a7d8c200
IMAD-HB <habbiimadeddine@gmail.com>
2025-07-24 13:02:20 +0100

    Fixed auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -63,8 +63,8 @@ export const login = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        secure: true,
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json({
@@ -287,8 +287,8 @@ export const settleDebt = async (req, res) => {
 export const logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    secure: true,
+    sameSite: "None",
   });
 
   res.status(200).json({ message: "تم تسجيل الخروج بنجاح" });
